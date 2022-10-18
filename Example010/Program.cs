01f3cacb01e93bbee21b9c20849baf3db97a8e01
Vazgen <egiazarov.vazgen@icloud.com>
2022-10-18 14:56:12 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int ShowSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = (num - firstDigit * 100) / 10;
+    return secondDigit;
+}
+
+int secondDigit = ShowSecondDigit(num);
+Console.WriteLine("Вторая цифра этого числа: " + secondDigit);
