581115c98936ed242d0de63237ab6963fcc3ecca
Vazgen <egiazarov.vazgen@icloud.com>
2022-10-18 16:48:25 +0300

    Example015

---
+++ Example015/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (multiplicity(num))
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это не выходной день");
+}
+
+bool multiplicity(int num)
+{
+    return 8 > num && num > 5;
+}
