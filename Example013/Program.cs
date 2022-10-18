53b688a9a35b311fdd280935b061ece1f489d178
Vazgen <egiazarov.vazgen@icloud.com>
2022-10-18 16:24:52 +0300

    Example013

---
+++ Example013/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num > 99)
+{
+    int ShowThirdDigit(int num)
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        int thirdDigit = num % 10;
+        return thirdDigit;
+    }
+    Console.WriteLine("Третья цифра заданного числа: " + ShowThirdDigit(num));
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
